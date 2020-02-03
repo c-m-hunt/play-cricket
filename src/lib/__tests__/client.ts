10b235e1bd97e5f4626d1bec9223a3d02b1b0d34
Chris Hunt <chris.hunt@hudl.com>
2020-02-03 11:14:16 +0000

    Add more tests. Add more details to package.json

--- src/lib/__tests__/client.ts
+++ src/lib/__tests__/client.ts
@@ -1,4 +1,4 @@
-import { Client, NotFoundError } from '../client';
+import { Client, NotFoundError, NotAuthorisedError } from '../client';
 
 const apiKey = 'abc123'
 
@@ -11,6 +11,9 @@ mockedAxios.get.mockResolvedValue({ data: "test" })
 mockedAxiosInst.create.mockReturnValue(mockedAxios)
 
 describe("Play Cricket client class", () => {
+  afterEach(() => {
+    mockedAxios.get.mockClear();
+  });
 
   test('Client class construction', () => {
     const c = new Client(apiKey)
@@ -24,6 +27,19 @@ describe("Play Cricket client class", () => {
     expect(data).toBe('test')
   })
 
+  test('all methods call request', async () => {
+    const c = new Client(apiKey)
+    const data1 = await c.getTeamsInComp(87298)
+    const data2 = await c.getLeagueTable(123)
+    const data3 = await c.getMatches(1234, 2020)
+    const data4 = await c.getMatchDetail(1234)
+    expect(mockedAxios.get).toBeCalledTimes(4)
+    expect(data1).toBe('test')
+    expect(data2).toBe('test')
+    expect(data3).toBe('test')
+    expect(data4).toBe('test')
+  })
+
   test('handles 404', async (done) => {
     mockedAxios.get.mockImplementation(() => {
       return Promise.reject({
@@ -35,6 +51,20 @@ describe("Play Cricket client class", () => {
       await c.getCompetitions(2018, 7300000, "cups")
     }
     await expect(t()).rejects.toThrow(NotFoundError)
+    done()
+  })
+
+  test('handles 401', async (done) => {
+    mockedAxios.get.mockImplementation(() => {
+      return Promise.reject({
+        response: {status: 401}
+      })
+    })
+    const t = async () => {
+      const c = new Client(apiKey)
+      await c.getCompetitions(2018, 7300000, "cups")
+    }
+    await expect(t()).rejects.toThrow(NotAuthorisedError)
     done()
   })
 })
