1ed91960a595044ef87f0b053958aac63a47a488
Chris Hunt <chris.hunt@hudl.com>
2020-08-21 18:05:01 +0100

    Fix bug with result summary response

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,61 +1,73 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import axios, { AxiosInstance } from 'axios'
-import { CompetitionTeamsResponse } from './interface/competitionTeams'
-import { CompetitionsResponse } from './interface/competitions'
-import { LeagueTableResponse } from './interface/leagueTable'
-import { MatchesResponse } from './interface/matches'
-import { MatchDetailResponse } from './interface/matchDetail'
-import { MatchQuery, ResultQuery } from './interface/general'
-import { convertMatchQueryToPCQuery } from './tools'
+import axios, { AxiosInstance } from "axios";
+import { CompetitionTeamsResponse } from "./interface/competitionTeams";
+import { CompetitionsResponse } from "./interface/competitions";
+import { LeagueTableResponse } from "./interface/leagueTable";
+import { MatchesResponse } from "./interface/matches";
+import { MatchDetailResponse } from "./interface/matchDetail";
+import { MatchQuery, ResultQuery } from "./interface/general";
+import { convertMatchQueryToPCQuery } from "./tools";
+import { ResultSummaryResponse } from "./interface/resultSummary";
 
 export class NotFoundError extends Error {}
 export class NotAuthorisedError extends Error {}
 
 export class Client {
-  public apiKey: string
-  private baseURL = `http://play-cricket.com/api/v2/`
-  private http: AxiosInstance
+  public apiKey: string;
+  private baseURL = `http://play-cricket.com/api/v2/`;
+  private http: AxiosInstance;
   public constructor(apiKey: string) {
-    this.apiKey = apiKey
+    this.apiKey = apiKey;
     this.http = axios.create({
-      baseURL: this.baseURL
-    })
+      baseURL: this.baseURL,
+    });
   }
 
   private request = async <T>(path: string, params: object): Promise<T> => {
-    params = { ...params, api_token: this.apiKey }
+    params = { ...params, api_token: this.apiKey };
     try {
       const response = await this.http.get(path, {
-        params
-      })
-      return response.data
+        params,
+      });
+      return response.data;
     } catch (ex) {
       if (ex.response.status === 404) {
-        throw new NotFoundError('Data not found')
+        throw new NotFoundError("Data not found");
       }
       if (ex.response.status === 401) {
-        throw new NotAuthorisedError(ex.message)
+        throw new NotAuthorisedError(ex.message);
       }
     }
-  }
+  };
 
-  public getTeamsInComp = async (id: number): Promise<CompetitionTeamsResponse> => {
-    return await this.request<CompetitionTeamsResponse>('competition_teams.json', { id })
-  }
+  public getTeamsInComp = async (
+    id: number,
+  ): Promise<CompetitionTeamsResponse> => {
+    return await this.request<CompetitionTeamsResponse>(
+      "competition_teams.json",
+      { id },
+    );
+  };
 
-  public getCompetitions = async (season: number, leagueId: number, competitionType: "divisions"|"cups"): Promise<CompetitionsResponse> => {
-    return await this.request<CompetitionsResponse>('competitions.json', {
+  public getCompetitions = async (
+    season: number,
+    leagueId: number,
+    competitionType: "divisions" | "cups",
+  ): Promise<CompetitionsResponse> => {
+    return await this.request<CompetitionsResponse>("competitions.json", {
       season,
       league_id: leagueId,
-      competition_type: competitionType
-    })
-  }
+      competition_type: competitionType,
+    });
+  };
 
-  public getLeagueTable = async (divId: number): Promise<LeagueTableResponse> => {
-    return await this.request<LeagueTableResponse>('league_table.json', {
-      division_id: divId
-    })
-  }
+  public getLeagueTable = async (
+    divId: number,
+  ): Promise<LeagueTableResponse> => {
+    return await this.request<LeagueTableResponse>("league_table.json", {
+      division_id: divId,
+    });
+  };
 
   public getMatches = async (
     siteId: number,
@@ -63,25 +75,25 @@ export class Client {
     query: MatchQuery | null = null,
   ): Promise<MatchesResponse> => {
     return this.getMatchesEndpoint<MatchesResponse, MatchQuery>(
-      'matches.json',
+      "matches.json",
       siteId,
       season,
-      query
-    )
-  }
+      query,
+    );
+  };
 
   public getResults = async (
     siteId: number,
     season: number,
     query: ResultQuery | null = null,
-  ): Promise<MatchesResponse> => {
-    return this.getMatchesEndpoint<MatchesResponse, ResultQuery>(
-      'result_summary.json',
+  ): Promise<ResultSummaryResponse> => {
+    return this.getMatchesEndpoint<ResultSummaryResponse, ResultQuery>(
+      "result_summary.json",
       siteId,
       season,
-      query
-    )
-  }
+      query,
+    );
+  };
 
   private getMatchesEndpoint = async <T, U>(
     endpoint: string,
@@ -89,21 +101,22 @@ export class Client {
     season: number,
     query: U | null = null,
   ): Promise<T> => {
-    let pcQuery = {}
+    let pcQuery = {};
     if (query) {
-      pcQuery = convertMatchQueryToPCQuery(query)
+      pcQuery = convertMatchQueryToPCQuery(query);
     }
     return await this.request<T>(endpoint, {
       site_id: siteId,
       season,
-      ...pcQuery
-    })
-  }
+      ...pcQuery,
+    });
+  };
 
-  public getMatchDetail = async (matchId: number): Promise<MatchDetailResponse> => {
-    return await this.request<MatchDetailResponse>('match_detail.json', {
-      match_id: matchId
-    })
-  }
-
+  public getMatchDetail = async (
+    matchId: number,
+  ): Promise<MatchDetailResponse> => {
+    return await this.request<MatchDetailResponse>("match_detail.json", {
+      match_id: matchId,
+    });
+  };
 }
--- src/lib/interface/matches.ts
+++ src/lib/interface/matches.ts
@@ -3,50 +3,50 @@ export interface MatchesResponse {
 }
 
 export interface Match {
-  id:               number;
-  status:           Status;
-  published:        Published;
-  last_updated:     string;
-  league_name:      string;
-  league_id:        string;
+  id: number;
+  status: Status;
+  published: Published;
+  last_updated: string;
+  league_name: string;
+  league_id: string;
   competition_name: string;
-  competition_id:   string;
+  competition_id: string;
   competition_type: CompetitionType;
-  match_type:       MatchType;
-  game_type:        GameType;
-  season:           string;
-  match_date:       string;
-  match_time:       string;
-  ground_name:      string;
-  ground_id:        string;
-  ground_latitude:  string;
+  match_type: MatchType;
+  game_type: GameType;
+  season: string;
+  match_date: string;
+  match_time: string;
+  ground_name: string;
+  ground_id: string;
+  ground_latitude: string;
   ground_longitude: string;
-  home_club_name:   string;
-  home_team_name:   string;
-  home_team_id:     string;
-  home_club_id:     string;
-  away_club_name:   string;
-  away_team_name:   string;
-  away_team_id:     string;
-  away_club_id:     string;
-  umpire_1_name:    string;
-  umpire_1_id:      string;
-  umpire_2_name:    string;
-  umpire_2_id:      string;
-  umpire_3_name:    string;
-  umpire_3_id:      string;
-  referee_name:     string;
-  referee_id:       string;
-  scorer_1_name:    string;
-  scorer_1_id:      string;
-  scorer_2_name:    string;
-  scorer_2_id:      string;
+  home_club_name: string;
+  home_team_name: string;
+  home_team_id: string;
+  home_club_id: string;
+  away_club_name: string;
+  away_team_name: string;
+  away_team_id: string;
+  away_club_id: string;
+  umpire_1_name: string;
+  umpire_1_id: string;
+  umpire_2_name: string;
+  umpire_2_id: string;
+  umpire_3_name: string;
+  umpire_3_id: string;
+  referee_name: string;
+  referee_id: string;
+  scorer_1_name: string;
+  scorer_1_id: string;
+  scorer_2_name: string;
+  scorer_2_id: string;
 }
 
 export enum CompetitionType {
   Cup = "Cup",
   League = "League",
-  Friendly = "Friendly"
+  Friendly = "Friendly",
 }
 
 export enum GameType {
@@ -59,7 +59,7 @@ export enum MatchType {
 
 export enum Published {
   Yes = "Yes",
-  No = "No"
+  No = "No",
 }
 
 export enum Status {
